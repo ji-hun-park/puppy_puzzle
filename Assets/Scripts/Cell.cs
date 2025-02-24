1e85b371357e10eb8a55a3d13a77cd69aec7e20a
JiHun <pgh979@gmail.com>
2025-02-24 20:07:52 +0900

    Add borad

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public enum Movable
+    {
+        Move,
+        Static
+    }
+    public enum CellColor
+    {
+        Light,
+        Red,
+        Blue,
+        Orange,
+        Brown,
+        Dark,
+        Green,
+        Yellow,
+        Violet,
+        Pink
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 싱글톤 패턴 적용
+    public static GameManager Instance;
+    public UIManager uiManager;
+    public Sprite[] colorSprites;
+
+    private void Awake()
+    {
+        // Instance 존재 유무에 따라 게임 매니저 파괴 여부 정함
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 기존에 존재 안하면 이걸로 대체하고 파괴하지 않기
+        }
+        else
+        {
+            Destroy(gameObject); // 기존에 존재하면 자신파괴
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
